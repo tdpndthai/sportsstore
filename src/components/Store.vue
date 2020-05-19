5fe5add0dac2d38fd3e605b45922ab8992025539
Nguyễn Danh Thái <47393239+tdpndthai@users.noreply.github.com>
2020-05-19 21:45:27 +0700

    init project
    sort by cartegiry,pagination,change pageSize

---
+++ src/components/Store.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col bg-dark text-white">
+                <a class="navbar-brand">SPORTS STORE</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3 bg-info p-2">
+                <CategoryControls/>
+            </div>
+            <div class="col-9 p-2 ">
+                <product-list/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ProductList from "./ProductList";
+    import CategoryControls from "./CategoryControls";
+
+    export default {
+        name: "Store",
+        components: {ProductList, CategoryControls}
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css"
+import store from "./store";
 
 Vue.config.productionTip = false
 
+
 new Vue({
   render: h => h(App),
+  store:store,
 }).$mount('#app')
